966043236f255b4ce55022360b9c9398218a0960
Quang Truong <votruong284@gmail.com>
2023-05-12 01:41:26 +0700

    first commit

--- src/models/user.js
+++ src/models/user.js
@@ -14,7 +14,7 @@ const userSchema = new Schema(
       type: String,
       required: [true, 'Password is required!'],
     },
-    dateOfBirth: {
+    dateBirth: {
       type: Date,
       required: [true, 'Date of birth is required!'],
     },
